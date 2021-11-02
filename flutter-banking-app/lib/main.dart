37de9a7a42539ad6d7772cb4d4b7458515e66a71
samkelo <samke704@gmail.com>
2021-11-02 23:09:29 +0200

    Work updated
    Basic Bank Flutter

---
+++ flutter-banking-app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:basic_banking/screens/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Home Page",
+      home: Home(),
+      theme: ThemeData(fontFamily: "Raleway"),
+    );
+  }
+}
